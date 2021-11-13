544de6df937f9039accab68e1b0addf1e2477310
Zannatul-Fahmida <jannatulfahmida15@gmail.com>
2021-11-13 12:27:46 +0600

    all orders added

--- src/pages/Dashboard/ManageToys/ManageToys.js
+++ src/pages/Dashboard/ManageToys/ManageToys.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Container, Image, Table, Button, Row, Spinner } from 'react-bootstrap';
+import { Container, Button, Row, Spinner } from 'react-bootstrap';
 import AllToys from '../AllToys/AllToys';
 
 const ManageToys = () => {
